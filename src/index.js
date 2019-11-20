6589c7829dd389c606c58c4eedda1fde1646017f
Erich Menezes <erich_menezes@gap.com>
2019-11-20 14:32:42 -0800

    :construction:

--- src/index.js
+++ src/index.js
@@ -2,31 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from "@reach/router"
 import App from './app';
-import Login from './login';
 import * as serviceWorker from './serviceWorker';
-
-function AuthenticatedRoute({ children, isAuth }) {
-  if (isAuth === true) {
-    return children;
-  } else {
-    return (
-      <div>
-        invalid login
-      </div>
-    )
-  }
-}
-
-const PrivateCategory = ({ location: { state: { isAuth } } }) => (
-    <AuthenticatedRoute isAuth={isAuth}>
-      <App/>
-    </AuthenticatedRoute>
-  );
 
 ReactDOM.render(
   <Router>
-    <Login path='/' default />
-    <PrivateCategory path='category'/>
+    <App path='category'/>
   </Router>
   , document.getElementById('root'));
 
